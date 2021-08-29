cbca0310638632add45438abc4ef7e5db714a96a
RiktoVai <89726125+RiktoVai@users.noreply.github.com>
2021-08-29 15:02:45 +0600

    html css and js

---
+++ index.js
@@ -0,0 +1,11 @@
+x = document.querySelector("title");
+a = document.querySelector(".cmt");
+b = document.querySelector(".aidt");
+c = document.querySelector(".rat");
+d = document.querySelector(".emt");
+
+x.innerHTML="BBPI";
+a.innerHTML="cmt";
+b.innerHTML="aidt";
+c.innerHTML="rat";
+d.innerHTML="emt";
